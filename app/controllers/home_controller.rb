77d4ca9154105d60dd1ef93be96aaeafb6d69503
Frank Wang <info@wangzifeng.com>
2014-02-20 00:40:29 +0800

    new commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+  def index
+  	@name = "wangzifeng"
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wangzifeng::Application.config.secret_token = 'a8571b9f1ab3148b920a440b522304283b297799b7580acfc8b9080a69c59ed9f333c909ddc642b315f275090349e35050ccc5550e8fab3e77f5105f122941c3'
